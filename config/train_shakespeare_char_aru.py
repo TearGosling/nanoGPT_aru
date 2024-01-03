86582a31209c2bc68d7ab82e1dd0d0656ed8b88f
TearGosling <teargoslingwaifu@gmail.com>
2024-01-03 10:19:27 -0600

    Bugfix and additional shakespeare configs
    Adds SwiGLU and ARU configs

--- config/train_shakespeare_char_aru.py
+++ config/train_shakespeare_char_aru.py
@@ -9,9 +9,9 @@ log_interval = 10 # don't print too too often
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-wandb_log = True # override via command line if you like
+wandb_log = False # override via command line if you like
 wandb_project = 'shakespeare-char'
-wandb_run_name = 'mini-gpt-gelu'
+wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
@@ -24,7 +24,7 @@ n_head = 6
 n_embd = 384
 dropout = 0.2
 gated = False
-act_fn = "gelu"
+act_fn = "aru"
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 max_iters = 5000
