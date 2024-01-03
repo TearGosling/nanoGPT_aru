4414d227a845e0bee02c97bcd6c3f1026e7b017f
TearGosling <teargoslingwaifu@gmail.com>
2024-01-03 09:58:41 -0600

    bugfix

--- model.py
+++ model.py
@@ -89,7 +89,7 @@ class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.act     = ACT_FUNCTIONS[config.act_fn]
+        self.act     = ACT_FUNCTIONS[config.act_fn]()
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
@@ -105,7 +105,7 @@ class GatedMLP(nn.Module):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
         self.c_gate  = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.act     = ACT_FUNCTIONS[config.act_fn]
+        self.act     = ACT_FUNCTIONS[config.act_fn]()
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
